046153bd808d08048901bd8019f0e0d5cfdc3cc4
gus07ven <gus07ven@users.noreply.github.com>
2016-09-28 23:29:41 -0500

    Add files via upload

---
+++ mipsVid11.asm
@@ -0,0 +1,15 @@
+.data
+	
+
+.text
+	addi $t0, $zero, 2000
+	addi $t1, $zero, 10
+	
+	mult $t0, $t1
+	
+	mflo $s0
+	
+	# Displays the product to the screen
+	li $v0, 1
+	add $a0, $zero, $s0
+	syscall
